5f0fc0c2075bd5d3a22db57bcff20568752efea2
Lazybaxy <halvard.erla@gmail.com>
2021-12-21 18:23:47 +0100

    test ray

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+#include <iostream>
+
+color ray_color(const ray& r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = .5f * (unit_direction.y() + 1.f);
+    return (1.f - t) * color(1.f, 1.f, 1.f) + t * color(.5f, .7f, 1.f);
+}
+
+int main() {
+
+    // Image
+    const float aspect_ratio = 16.f / 9.f;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+
+    // Camera
+    
+    auto viewport_height = 2.f;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.f;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto ll_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+
+            auto u = float(i) / (image_width - 1);
+            auto v = float(j) / (image_height - 1);
+            ray r(origin, ll_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
+
+            //color pixel_color(float(i) / (image_width - 1), float(j) / (image_height - 1), .25f);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
